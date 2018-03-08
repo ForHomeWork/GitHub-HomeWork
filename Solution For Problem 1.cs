7d4ffa4b3fb9c03a2bf24c757cff2a7466187ea4
Smruck <smruck@gmail.com>
2018-03-08 12:41:27 +0200

    adding a solution for Debit Card Number

---
+++ Solution For Problem 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Numerics;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            int i = 4;
+            while (i-->0)
+            {
+                string input = Console.ReadLine().PadLeft(4, '0');
+                list.Add(input);
+            }
+            Console.WriteLine(string.Join(" ",list));
+        }
+    }
+}
